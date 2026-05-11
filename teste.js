ba23b7cacd886188d653656930a885338ab57c40
Anny <annycostavitoria@gmail.com>
2026-05-11 17:21:22 -0300

    Remove express do package.json e atualiza o arquivo teste.js

--- teste.js
+++ teste.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const BibliotecaUsuarios = require('./src/database/bibliotecaUsuarios');
 const BibliotecaPosts = require('./src/database/bibliotecaPosts');
+const BibliotecaComentarios = require('./src/database/bibliotecaComentarios');
 
 async function executarTestes() {
     try {
@@ -21,11 +22,24 @@ async function executarTestes() {
         });
         console.log(`Receita publicada: ${novaReceita.texto}`);
 
+        const novoComentario = await BibliotecaComentarios.criar({
+            texto: "Fiz ontem e ficou deliciosa!",
+            postagemId: novaReceita._id,
+            autorId: novoUsuario._id
+        });
+        console.log(`Comentário publicado: ${novoComentario.texto}`);
+
         console.log("\n--- TESTANDO BUSCA ---");
         const todasReceitas = await BibliotecaPosts.buscarTodos();
         console.log(`Foram encontradas ${todasReceitas.length} receitas no banco.`);
+        
+        const todosComentarios = await BibliotecaComentarios.buscarTodos();
+        console.log(`Foram encontrados ${todosComentarios.length} comentários no banco.`);
 
         console.log("\n--- TESTANDO DELEÇÃO ---");
+        await BibliotecaComentarios.remover(novoComentario._id);
+        console.log("Comentário apagado com sucesso.");
+        
         await BibliotecaPosts.remover(novaReceita._id);
         console.log("Receita apagada com sucesso do banco.");
 
